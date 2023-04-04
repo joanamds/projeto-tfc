43668060372dcc20ad7f787781e134ba87bd9d41
joanamds <joanamdsantos1@gmail.com>
2023-04-04 13:14:29 -0300

    :hammer: refactor usando ORM nas funções do service

--- app/backend/src/database/services/leaderboard.service.ts
+++ app/backend/src/database/services/leaderboard.service.ts
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 import TeamsModel from '../models/TeamsModel';
 import MatchesModel from '../models/MatchesModel';
 
+const m = 'matches.home_team_id';
 export default class LeaderboardService {
   private sequelize: Sequelize;
   private teamsModel: typeof TeamsModel;
@@ -18,172 +19,179 @@ export default class LeaderboardService {
   }
 
   public async totalGames(): Promise<{ name: string, totalGames: number }[]> {
-    const result = await this.teamsModel.findAll({
+    const result = await this.matchesModel.findAll({
       attributes: [
-        'teamName',
-        [this.sequelize.fn('COUNT', this.sequelize.col('matches.id')), 'totalGames'],
+        [this.sequelize.fn('COUNT', this.sequelize.col(m)), 'totalGames'],
       ],
       include: [{
-        model: this.matchesModel,
-        where: { inProgress: false },
-        attributes: [],
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
       }],
-      group: ['teams.id'],
+      where: { inProgress: false },
+      group: [m],
     });
     return result.map((row) => ({
-      name: row.teamName,
+      name: row.homeTeam.teamName,
       totalGames: row.get('totalGames') as number,
     }));
   }
 
   public async totalVictories(): Promise<{ name: string, totalVictories: number }[]> {
-    const result = await this.sequelize.query(
-      `SELECT teams.team_name AS name,
-      SUM(CASE
-      WHEN matches.home_team_id = teams.id
-      AND matches.home_team_goals > matches.away_team_goals THEN 1
-      WHEN matches.away_team_id = teams.id
-      AND matches.away_team_goals > matches.home_team_goals THEN 1
-      ELSE 0
-      END) AS totalVictories
-      FROM teams JOIN matches
-      ON teams.id = matches.home_team_id
-      OR teams.id = matches.away_team_id AND matches.in_progress = false
-      WHERE matches.in_progress = false
-      GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-    return result as { name: string, totalVictories: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.fn('SUM', this.sequelize.literal(
+        `CASE WHEN matches.home_team_id = homeTeam.id
+        AND matches.home_team_goals > matches.away_team_goals THEN 1
+        ELSE 0 END`,
+      )), 'totalVictories']],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      }],
+      where: { inProgress: false },
+      group: [m],
+    });
+    return result.map((row) => ({
+      name: row.homeTeam.teamName,
+      totalVictories: row.get('totalVictories') as number,
+    }));
   }
 
   public async totalDraws(): Promise<{ name: string, totalDraws: number }[]> {
-    const result = await this.sequelize.query(
-      `
-      SELECT  teams.team_name AS name, SUM(CASE
-      WHEN matches.home_team_goals = matches.away_team_goals THEN 1
-      ELSE 0
-      END) AS totalDraws
-      FROM teams
-      JOIN matches
-      ON teams.id = matches.home_team_id
-      OR teams.id = matches.away_team_id AND matches.in_progress = false
-      GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-
-    return result as { name: string, totalDraws: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.fn('SUM', this.sequelize.literal(
+        `CASE WHEN matches.home_team_id = homeTeam.id
+        AND matches.home_team_goals = matches.away_team_goals THEN 1
+        ELSE 0 END`,
+      )), 'totalDraws']],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      }],
+      where: { inProgress: false },
+      group: [m],
+    });
+    return result.map((row) => ({
+      name: row.homeTeam.teamName,
+      totalDraws: row.get('totalDraws') as number,
+    }));
   }
 
   public async totalLosses(): Promise<{ name: string, totalLosses: number }[]> {
-    const result = await this.sequelize.query(
-      `
-      SELECT  teams.team_name AS name, SUM(CASE
-      WHEN matches.home_team_id = teams.id
-      AND matches.home_team_goals < matches.away_team_goals THEN 1
-      WHEN matches.away_team_id = teams.id
-      AND matches.away_team_goals < matches.home_team_goals THEN 1
-      ELSE 0
-      END) AS totalLosses
-      FROM teams
-      JOIN matches
-      ON teams.id = matches.home_team_id
-      OR teams.id = matches.away_team_id AND matches.in_progress = false
-      GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-    return result as { name: string, totalLosses: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.fn('SUM', this.sequelize.literal(
+        `CASE WHEN matches.home_team_id = homeTeam.id
+        AND matches.home_team_goals < matches.away_team_goals THEN 1
+        ELSE 0 END`,
+      )), 'totalLosses']],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      }],
+      where: { inProgress: false },
+      group: [m],
+    });
+    return result.map((row) => ({
+      name: row.homeTeam.teamName,
+      totalLosses: row.get('totalLosses') as number,
+    }));
   }
 
   public async goalsFavor(): Promise<{ name: string, goalsFavor: number }[]> {
-    const result = await this.sequelize.query(
-      `
-      SELECT  teams.team_name AS name, SUM(CASE
-      WHEN matches.home_team_id = teams.id THEN matches.home_team_goals
-      ELSE matches.away_team_goals
-      END) AS goalsFavor
-      FROM teams
-      JOIN matches
-      ON teams.id = matches.home_team_id
-      OR teams.id = matches.away_team_id AND matches.in_progress = false
-      GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-    return result as { name: string, goalsFavor: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.fn('SUM', this.sequelize.literal(
+        `CASE WHEN matches.home_team_id = homeTeam.id
+          THEN matches.home_team_goals
+        END`,
+      )), 'goalsFavor']],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      }],
+      where: { inProgress: false },
+      group: [m],
+    });
+    return result.map((row) => ({
+      name: row.homeTeam.teamName,
+      goalsFavor: row.get('goalsFavor') as number,
+    }));
   }
 
   public async goalsOwn(): Promise<{ name: string, goalsOwn: number }[]> {
-    const result = await this.sequelize.query(
-      `
-      SELECT  teams.team_name AS name, SUM(CASE
-      WHEN matches.home_team_id = teams.id THEN matches.away_team_goals
-      ELSE matches.home_team_goals
-      END) AS goalsOwn
-      FROM teams
-      JOIN matches
-      ON teams.id = matches.home_team_id
-      OR teams.id = matches.away_team_id AND matches.in_progress = false
-      GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-    return result as { name: string, goalsOwn: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.fn('SUM', this.sequelize.literal(
+        `CASE WHEN matches.home_team_id = homeTeam.id
+          THEN matches.away_team_goals
+        END`,
+      )), 'goalsOwn']],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      }],
+      where: { inProgress: false },
+      group: [m],
+    });
+    return result.map((row) => ({
+      name: row.homeTeam.teamName,
+      goalsOwn: row.get('goalsOwn') as number,
+    }));
   }
 
   public async goalsBalance(): Promise<{ name: string, goalsBalance: number }[]> {
-    const result = await this.sequelize.query(
-      `
-      SELECT  teams.team_name AS name, SUM(CASE
-      WHEN matches.home_team_id = teams.id THEN matches.home_team_goals
-      ELSE matches.away_team_goals
-      END) - SUM(CASE
-      WHEN matches.home_team_id = teams.id THEN matches.away_team_goals
-      ELSE matches.home_team_goals
-      END) AS goalsBalance
-      FROM teams
-      JOIN matches
-      ON teams.id = matches.home_team_id
-      OR teams.id = matches.away_team_id AND matches.in_progress = false
-      GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-    return result as { name: string, goalsBalance: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.fn('SUM', this.sequelize.literal(
+        `CASE WHEN matches.home_team_id = homeTeam.id THEN matches.home_team_goals
+        ELSE matches.away_team_goals END) - SUM(CASE WHEN matches.home_team_id = homeTeam.id 
+          THEN matches.away_team_goals ELSE matches.home_team_goals END`,
+      )), 'goalsBalance']],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: ['teamName'],
+      }],
+      where: { inProgress: false },
+      group: [m],
+    });
+    return result.map((row) => ({
+      name: row.homeTeam.teamName,
+      goalsBalance: row.get('goalsBalance') as number,
+    }));
   }
 
   public async efficiency(): Promise<{ name: string, efficiency: number }[]> {
-    const result = await this.sequelize.query(
-      `
-      SELECT  teams.team_name AS name, ROUND(
-        (SUM(CASE WHEN matches.home_team_id = teams.id
-          AND matches.home_team_goals > matches.away_team_goals THEN 1
-          WHEN matches.away_team_id = teams.id
-          AND matches.away_team_goals > matches.home_team_goals THEN 1 ELSE 0 END) * 3
-        + SUM(CASE WHEN matches.home_team_goals = matches.away_team_goals THEN 1
-        ELSE 0 END)) / COUNT(*) * 100, 2) AS efficiency
-    FROM TRYBE_FUTEBOL_CLUBE.teams AS teams
-    JOIN TRYBE_FUTEBOL_CLUBE.matches AS matches
-    ON teams.id = matches.home_team_id OR teams.id = matches.away_team_id
-    WHERE matches.in_progress = false
-    GROUP BY teams.id;
-      `,
-      { type: 'SELECT' },
-    );
-    return result as { name: string, efficiency: number }[];
+    const result = await this.matchesModel.findAll({
+      attributes: [[this.sequelize.literal('`homeTeam`.`team_name`'), 'name'],
+        [this.sequelize.fn('ROUND', this.sequelize.literal(`(SUM(CASE
+          WHEN matches.home_team_id = homeTeam.id
+          AND matches.home_team_goals > matches.away_team_goals THEN 1 ELSE 0 END)) /
+          COUNT(matches.home_team_id) * 100`), 2), 'efficiency'],
+      ],
+      include: [{
+        model: this.teamsModel,
+        as: 'homeTeam',
+        attributes: [],
+      }],
+      where: { inProgress: false },
+      group: ['homeTeam.team_name'],
+    });
+    return result.map((row) => ({ name: row.get('name') as string,
+      efficiency: row.get('efficiency') as number,
+    }));
   }
 
   public async calculatePoints(): Promise<{ name: string, totalPoints: number }[]> {
-    const [victories, draws, losses] = await Promise.all([
+    const [victories, draws] = await Promise.all([
       this.totalVictories(),
       this.totalDraws(),
-      this.totalLosses(),
     ]);
     const points = victories.map((v, i) => ({
       name: v.name,
-      totalPoints: v.totalVictories * 3 + draws[i].totalDraws * 1 + losses[i].totalLosses * 0,
+      totalPoints: v.totalVictories * 3 + draws[i].totalDraws * 1,
     }));
 
     return points as { name: string, totalPoints: number }[];
